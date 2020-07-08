4d3e8854f50cbe9ec979fe12be6085210b391de5
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2020-07-08 16:32:41 -0300

    Adição de scroll suave

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ $(document).ready(function () {
 
 })
 
+$('.navmenu a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    let id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+      
+    $('html, body').animate({ 
+      scrollTop: targetOffset - 100
+    }, 500);
+  });
